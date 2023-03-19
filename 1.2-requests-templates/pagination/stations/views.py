8eebd0c58750c352889869c4de4bfc394efab7b6
AndreyDyv <andreudyakov@gmail.com>
2023-03-20 00:30:33 +0400

    - views.py - добавлена переменная DATA, в bus_stations прописана пагинация
    - index.html - перенес блок <!DOCTYPE html> на 1 строку документа, чтобы PyCharm не ругался

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,7 @@
+from csv import DictReader
+
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -6,12 +10,23 @@ def index(request):
     return redirect(reverse('bus_stations'))
 
 
+DATA = []
+
+with open(settings.BUS_STATION_CSV, newline='', encoding='UTF-8') as csvfile:
+    reader = DictReader(csvfile)
+    for row in reader:
+        DATA.append(row)
+
+
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+
+    paginator = Paginator(DATA, 10)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page.object_list,
+        'page': page,
     }
+
     return render(request, 'stations/index.html', context)
