5426ac2bfc06c71a4906f5770f2c44ec6caf32a3
AndreyDyv <andreudyakov@gmail.com>
2023-03-28 18:43:17 +0400

    views.py - def books_view books_list.html - цикл for

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
     return render(request, template, context)
+
+
+def sort_by_date(request, pub_date):
+    template = 'books/books_list.html'
+
+    context = {
+
+    }
+    return render(request, template, context)
+
+
