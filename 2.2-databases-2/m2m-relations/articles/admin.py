3f710aa5a19671f3d5f7b4c86a19e993d316c859
AndreyDyv <andreudyakov@gmail.com>
2023-04-19 18:48:48 +0400

    finished views.py - def articles_list() admin.py - создал модели по подсказкам, def clean()

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,16 +1,38 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Tag, Scope
 
 
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                counter += 1
+        if counter > 1:
+            raise ValidationError('Только один тэг может быть отмечен как главный')
+        elif counter == 0:
+            raise ValidationError('Отсутствует главный тэг')
+        return super().clean()
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name']
 
 @admin.register(Scope)
 class ScopeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['is_main']
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 1
+    formset = ScopeInlineFormset
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+    list_display = ['title', 'text', 'published_at', 'image']
+
