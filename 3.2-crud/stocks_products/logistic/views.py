77165827edda0a6094fa5c1ad1590999a7233cec
AndreyDyv <andreudyakov@gmail.com>
2023-05-12 16:10:56 +0400

    views.py фильтры, класс-пагинатор

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,10 +10,21 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['title', 'description']
+
+    pagination_class = PageNumberPagination
+
     # при необходимости добавьте параметры фильтрации
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products',]
+
+    pagination_class = PageNumberPagination
     # при необходимости добавьте параметры фильтрации
