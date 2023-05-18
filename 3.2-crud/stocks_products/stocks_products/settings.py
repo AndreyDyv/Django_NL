f88237dfac0fa3b2edc7401cb76cc0c6bb5e179c
AndreyDyv <andreudyakov@gmail.com>
2023-05-18 22:37:27 +0400

    settings.py del 'import rest_framework.pagination'  str15

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import rest_framework.pagination
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
