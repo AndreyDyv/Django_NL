f21566bbf9e1fe6edf01a1c70bcc7b7751a81619
AndreyDyv <andreudyakov@gmail.com>
2023-05-26 07:55:45 +0400

    views.py основные настройки ModelViewSet  + @action() но не понимаю какой код писать для методов get и post, откуда брать qeryset для сериализации

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,24 @@
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from advertisements.filters import AdvertisementFilter
 
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    permission_classes = (IsAuthenticated)
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
         return []
+
+    @action(methods=('GET','POST'), detail=False, url_path='fav')
+    def favourite(self, request, *args, **kwargs):
+        ...
