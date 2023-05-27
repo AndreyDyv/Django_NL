0cfa969cfad92a4908498b54d7be4347e398d73c
AndreyDyv <andreudyakov@gmail.com>
2023-05-27 15:55:25 +0400

    models.py добавил модель Favourites + миграция

--- 3.3-permissions/api_with_restrictions/advertisements/models.py
+++ 3.3-permissions/api_with_restrictions/advertisements/models.py
@@ -7,6 +7,7 @@ class AdvertisementStatusChoices(models.TextChoices):
 
     OPEN = "OPEN", "Открыто"
     CLOSED = "CLOSED", "Закрыто"
+    DRAFT = "DRAFT", "Черновик"
 
 
 class Advertisement(models.Model):
@@ -28,3 +29,14 @@ class Advertisement(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True
     )
+
+class Favourites(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    advertisement = models.ForeignKey(
+        Advertisement,
+        on_delete=models.CASCADE,
+    )
+
